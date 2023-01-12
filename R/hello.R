9be9bfd7b9f38d84bd830adb195ec8da659d37d1
Zhuleievan <zhulei_evan@126.com>
2023-01-12 17:52:53 +0800

    first commit

---
+++ R/hello.R
@@ -0,0 +1,33 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+flattenCPMatrix.zl <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+
+flattenCorrMatrix.zl <- function(cormat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    value =(cormat)[ut]
+  )
+}
