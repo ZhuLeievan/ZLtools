d6096d90cc546d0d44eccedd1b1015118c5a4354
Lei Zhu <119106978+ZhuLeievan@users.noreply.github.com>
2023-01-16 20:03:08 +0800

    Update core.R

--- R/core.R
+++ R/core.R
@@ -1,4 +1,4 @@
-core <- function(otu, num){
+zl.core <- function(otu, num){
   cutoff = num
   gene_filter <- data.frame(otu[which(apply(otu, 1, function(x){length(which(x!= 0))/length(x)}) >= cutoff),])
   }
